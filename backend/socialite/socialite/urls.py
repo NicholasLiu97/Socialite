82d32167442791ca61db2d2af8cd04d62f0835db
Nicholas <nicholasliu097@gmail.com>
2022-01-08 02:17:09 +0800

    create user login page

--- backend/socialite/socialite/urls.py
+++ backend/socialite/socialite/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('homepage/', include('homepage.urls'))
+    path('homepage/', include('homepage.urls')),
+    path('users/', include('users.urls'))
 ]
 
 if settings.DEBUG:
