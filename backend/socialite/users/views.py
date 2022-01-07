1f29a17fd916ab6ebb1416cc073b4998b42decb2
leowyh <leowyh4@gmail.com>
2022-01-08 04:36:02 +0800

    Debugged redirect to homepage

--- backend/socialite/users/views.py
+++ backend/socialite/users/views.py
@@ -1,33 +1,37 @@
 from django.contrib.auth import authenticate, login, logout
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
 # Create your views here.
 # displays info about current signed in user
+
+
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('users:login'))
     # ------------------------------- how to redirect to homepage ---------------------------------
     # return render(request, "users/user.html")
-    return render(request, "homepage/index.html")
+    return redirect('homepage:index')
+
 
 def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        user = authenticate(request, username=username, password= password)
-        if user is not None: # authentication successful
+        user = authenticate(request, username=username, password=password)
+        if user is not None:  # authentication successful
             login(request, user)
             # ------------------------------- how to redirect to homepage ---------------------------------
             # return HttpResponseRedirect(reverse("users:index"))
-            return HttpResponseRedirect("homepage/index.html")
+            return redirect('homepage:index')
 
         else:
             return render(request, "users/login.html", context={
                 "message": "invalid credentials"
             })
     return render(request, "users/login.html")
+
 
 def logout_view(request):
     logout(request)
